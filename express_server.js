569aa3fed2691eea8aa337bc88a8ac269966b478
emi-hi <emily.hillier@gmail.com>
2019-09-30 21:48:39 +0000

    added views

--- express_server.js
+++ express_server.js
@@ -12,8 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {

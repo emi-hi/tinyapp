6eb144762cf4d880fd217912c9f9146e163bfd16
emi-hi <emily.hillier@gmail.com>
2019-10-01 22:29:34 +0000

    added cookies for username

--- express_server.js
+++ express_server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 
 function generateRandomString() {
   let result = '';
@@ -28,13 +29,16 @@ app.get("/", (req, res) => {
 
 // URLS index
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 // add a new URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 //EDIT THE LONG VERSION
@@ -45,9 +49,14 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
+app.post("/urls/logout", (req, res) => {
+  res.clearCookie("username", req.cookies["username"] )
+  res.redirect("/urls")
+})
+
 // page with info on specific URL! Short and long
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -58,6 +67,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortened}`)
 
 });
+//when a user logs in, redirect to the homepage!
+app.post("/login", (req, res) => {
+  username = req.body["username"]
+  res.cookie("username", username)
+  res.redirect('/urls')
+})
 
 //if user goes to the shortened URL, redirect to the long version! 
 app.get("/u/:shortURL", (req, res) => {
